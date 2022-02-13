78566cbaa3f8dd25817e19cd64adab4c395741e6
Vladimir Pichugin <vladimir@pichug.in>
2022-02-13 20:38:39 +0300

    Update settings example

--- settings_example.py
+++ settings_example.py
@@ -6,24 +6,24 @@ class Settings:
     DEBUG = False
 
     MONGO = ''
-    MONGO_DATABASE = ''
-    MONGO_C_INVOICES = ''
-    MONGO_C_CLIENTS = ''
-    MONGO_C_MESSAGES = ''
-    MONGO_C_AUTO_INVOICES = ''
+    MONGO_DATABASE = 'invoices'
+    MONGO_C_INVOICES = 'invoices'
+    MONGO_C_CLIENTS = 'clients'
+    MONGO_C_MESSAGES = 'messages'
+    MONGO_C_AUTO_INVOICES = 'auto_invoices'
 
-    SMTP_SERVER_HOST = ''
-    SMTP_SERVER_PORT = 0
-    SMTP_CONN_TIMEOUT = 0
+    SMTP_SERVER_HOST = 'mail.pichug.in'
+    SMTP_SERVER_PORT = 465
+    SMTP_CONN_TIMEOUT = 5
     SMTP_SSL_CAFILE = 'assets/cert.pem'
 
-    SMTP_NAME = ''
+    SMTP_NAME = 'Vladimir Pichugin'
 
-    SMTP_RECEIPT_USER = ''
+    SMTP_RECEIPT_USER = 'receipt@pichug.in'
     SMTP_RECEIPT_PASS = ''
     SMTP_RECEIPT_HEADER_SERVICE = 'invoice_receipt'
 
-    SMTP_ALERT_USER = ''
+    SMTP_ALERT_USER = 'receipt@pichug.in'
     SMTP_ALERT_PASS = ''
     SMTP_ALERT_HEADER_SERVICE = 'invoice_notify'
 
@@ -41,5 +41,21 @@ class Settings:
             'date_created', 'date_due', 'date_paid', 'first_name', 'email',
             'preview', 'message_id', 'year'
         ]
+    }
+
+    INVOICE_TEMPLATE = {
+        'currency': 'RUB',
+        'status': None,
+        'created': None,
+        'due': None,
+        'payer': None,
+        'payee': None,
+        'gateways': [],
+        'items': [],
+        'transactions': [],
+        '_hidden': False,
+        '_version': 2,
+        'name': None,
+        'paid_timestamp': None
     }
 
